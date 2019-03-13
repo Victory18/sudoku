d0f488a66a5ab8256dd1a23c842c4d314c2a04a8
raccooccoon <zavadskaya_91@mail.ru>
2019-03-14 01:14:40 +0300

    #9

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-module.exports = function find(m, x, y) {
-  var n = [0,0,0,0,0,0,0,0,0,0];
 
-  for (var i = 0; i < 9; i++) {
-    n[m[y][i]]++;
-    n[m[i][x]]++;
-    n[m[(3 * ((y/3)|0))+((i/3)|0)][(3 * ((x/3)|0))+(i%3)]]++;
-  }
-
-  if (n.indexOf(0) === n.lastIndexOf(0)) return n.indexOf(0);
-
-  return 0;
-
-}
 
 module.exports = function solveSudoku(matrix) {
   do {
@@ -19,7 +6,21 @@ module.exports = function solveSudoku(matrix) {
     for (var y = 0; y < 9; y++) {
       for (var x = 0; x < 9; x++) {
         if (matrix[y][x] === 0) {
-          matrix[y][x] = find(matrix, x, y);
+          matrix[y][x] = function find(_matrix, x, y) {
+            var n = [0,0,0,0,0,0,0,0,0,0];
+          
+            for (var i = 0; i < 9; i++) {
+              n[m[y][i]]++;
+              n[m[i][x]]++;
+              n[m[(3 * ((y/3)|0))+((i/3)|0)][(3 * ((x/3)|0))+(i%3)]]++;
+            }
+          
+            if (n.indexOf(0) === n.lastIndexOf(0)) return n.indexOf(0);
+          
+            return 0;
+          
+          }
+
           gap |= matrix[y][x];
         }
       }
